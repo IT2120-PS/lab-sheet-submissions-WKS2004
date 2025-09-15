f1053d9bf9f27f1e76e38353b48f3f44d1c33a55
Wanshaja Sooriyabandara <159720575+WKS2004@users.noreply.github.com>
2025-09-15 18:34:14 +0530

    IT24102798 - Lab 06 - Discrete Probability Distributions

---
+++ Lab Sheet 06/IT24102798.R
@@ -0,0 +1,18 @@
+getwd()
+setwd("D:\\SLIIT\\Y2S1\\Modules\\IT2120 - Probability and Statistics\\Lab Submissions\\Week 06\\IT24102798")
+getwd()
+
+
+# Question 01
+
+#  (ii)
+
+pbinom(46, 50, 0.85, lower.tail = FALSE)
+
+
+# Question 02
+
+# (iii)
+
+dpois(15, 12)
+
