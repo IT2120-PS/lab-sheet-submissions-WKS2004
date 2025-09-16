aa2411c0d480d51c6b2f21659687aa8ad25a39e0
Wanshaja Sooriyabandara <159720575+WKS2004@users.noreply.github.com>
2025-09-16 21:14:22 +0530

    IT24102798 - Lab 07 - Continuous Probability Distributions

---
+++ Lab Sheet 07/IT24102798.R
@@ -0,0 +1,26 @@
+getwd()
+setwd("D:\\SLIIT\\Y2S1\\Modules\\IT2120 - Probability and Statistics\\Lab Submissions\\Week 07\\IT24102798")
+getwd()
+
+
+# Question 01
+
+punif(25, min = 0, max = 40, lower.tail = TRUE) - punif(10, min = 0, max = 40, lower.tail = TRUE)
+
+
+# Question 02
+
+pexp(2, rate = 1/3, lower.tail = TRUE)
+
+
+# Question 03
+
+# (i)
+
+pnorm(130, mean = 100, sd = 15, lower.tail = FALSE)
+
+
+# (ii)
+
+qnorm(0.95, mean = 100, sd = 15, lower.tail = TRUE)
+
